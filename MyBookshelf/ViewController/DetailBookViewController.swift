d3bf34ed9308a8107e324bc0290f87d4f5a54bfe
Hyejun Jang <jun@estmob.com>
2019-07-29 17:55:28 +0900

    Add DetailViewController

---
+++ MyBookshelf/ViewController/DetailBookViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailBookViewController.swift
+//  MyBookshelf
+//
+//  Created by 장혜준 on 29/07/2019.
+//  Copyright © 2019 장혜준. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailBookViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
